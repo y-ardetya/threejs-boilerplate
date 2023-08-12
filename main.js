76b3b37c3e0585319c9069bcbf29ba57eef19935
Yoga Ardli Ardetya <119592662+y-ardetya@users.noreply.github.com>
2023-08-12 12:54:24 +0300

    adding yuka

--- main.js
+++ main.js
@@ -7,36 +7,40 @@ export default class Sketch {
   constructor(options) {
     this.container = options.domElement;
     this.scene = new THREE.Scene();
-
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
-
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(this.width, this.height);
     this.container.appendChild(this.renderer.domElement);
 
+    //* GLTF Loader
     this.loader = new GLTFLoader();
 
-    this.light = new THREE.AmbientLight(0xffffff, 1);
-    this.scene.add(this.light);
-
+    //* Lights
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
     this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    this.directionalLight.position.set(0, 0, 1);
+    this.directionalLight.position.set(0, 0, 5);
+    this.scene.add(this.ambientLight);
     this.scene.add(this.directionalLight);
 
+    //* Camera
     this.camera = new THREE.PerspectiveCamera(
       45,
       this.width / this.height,
       0.1,
       1000
     );
-    this.camera.position.set(0, 20, 0);
+    this.camera.position.set(0, 10, 20);
     this.camera.lookAt(this.scene.position);
 
+    //* Orbit Controls
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
+    //* Times
     this.time = 0;
+    this.clock = new THREE.Clock();
 
+    //* Functions
     this.addObjects();
     this.setupResize();
     this.render();
@@ -56,36 +60,24 @@ export default class Sketch {
   }
 
   addObjects() {
-    // //* Geometry
-    // this.geometry = new THREE.ConeGeometry(0.2, 1, 8);
-    // this.geometry.rotateX(Math.PI / 2);
-    // //* Material
-    // this.material = new THREE.MeshNormalMaterial();
-    // //* Mesh
-    // this.mesh = new THREE.Mesh(this.geometry, this.material);
-    // //! Turning off mesh matrix auto update for Yuka
-    // this.mesh.matrixAutoUpdate = false;
-    // this.scene.add(this.mesh);
-
     //* Yuka Initialization
     this.vehicle = new YUKA.Vehicle();
-    // this.vehicle.setRenderComponent(this.mesh, sync);
 
+    //* Synchronize Yuka with model
     function sync(entity, renderComponent) {
       renderComponent.matrix.copy(entity.worldMatrix);
     }
 
     //* Yuka Path
     this.path = new YUKA.Path();
-    this.path.add(new YUKA.Vector3(-4, 0, 4));
-    this.path.add(new YUKA.Vector3(-6, 0, 0));
-    this.path.add(new YUKA.Vector3(-4, 0, -4));
+    this.path.add(new YUKA.Vector3(-6, 0, 4));
+    this.path.add(new YUKA.Vector3(-12, 0, 0));
+    this.path.add(new YUKA.Vector3(-6, 0, -12));
     this.path.add(new YUKA.Vector3(0, 0, 0));
-    this.path.add(new YUKA.Vector3(4, 0, -4));
-    this.path.add(new YUKA.Vector3(6, 0, 0));
+    this.path.add(new YUKA.Vector3(8, 0, -8));
+    this.path.add(new YUKA.Vector3(10, 0, 0));
     this.path.add(new YUKA.Vector3(4, 0, 4));
     this.path.add(new YUKA.Vector3(0, 0, 6));
-
     this.path.loop = true;
     this.vehicle.position.copy(this.path.current());
 
@@ -95,23 +87,22 @@ export default class Sketch {
       this.waypoint = this.path._waypoints[i];
       this.position.push(this.waypoint.x, this.waypoint.y, this.waypoint.z);
     }
-
     this.lineGeometry = new THREE.BufferGeometry();
     this.lineGeometry.setAttribute(
       "position",
       new THREE.Float32BufferAttribute(this.position, 3)
     );
+
     this.lineMaterial = new THREE.LineBasicMaterial({});
     this.line = new THREE.LineLoop(this.lineGeometry, this.lineMaterial);
     // this.scene.add(this.line);
 
     //* Yuka Follow Path Behavior
-    // this.mesh.position.copy(this.path.current());
-    this.followPathBehavior = new YUKA.FollowPathBehavior(this.path, 0.5);
+    this.followPathBehavior = new YUKA.FollowPathBehavior(this.path, 3);
     this.vehicle.steering.add(this.followPathBehavior);
 
-    this.onPathBehavior = new YUKA.OnPathBehavior(this.path);
-    this.vehicle.steering.add(this.onPathBehavior);
+    // this.onPathBehavior = new YUKA.OnPathBehavior(this.path);
+    // this.vehicle.steering.add(this.onPathBehavior);
 
     this.vehicle.maxSpeed = 3;
     //* Yuka Seek Behavior
@@ -120,19 +111,31 @@ export default class Sketch {
     //* Yuka Clock
     this.clock = new YUKA.Time();
 
-    this.loader.load("/assets/Fish.glb", (gltf) => {
+    //* Model Initialization
+    this.loader.load("/assets/clown.glb", (gltf) => {
       this.model = gltf.scene;
-      // gltf.scene.scale.set(0.2, 0.2, 0.2);
       this.scene.add(this.model);
       this.model.matrixAutoUpdate = false;
-      this.vehicle.scale = new YUKA.Vector3(0.2, 0.2, 0.2);
+      this.vehicle.scale = new YUKA.Vector3(0.02, 0.02, 0.02);
       this.vehicle.setRenderComponent(this.model, sync);
+
+      this.animation = {};
+      this.animation.mixer = new THREE.AnimationMixer(this.model);
+      this.animation.action = this.animation.mixer.clipAction(
+        gltf.animations[0]
+      );
+      this.animation.action.play();
+      this.render();
     });
   }
   render() {
+    this.time = this.clock.getDelta();
     //* Yuka Update
     this.delta = this.clock.update().getDelta();
     this.entityManager.update(this.delta);
+
+    //* Play Animation
+    this.animation.mixer.update(this.time);
 
     //* THREEJS Render
     this.renderer.render(this.scene, this.camera);
---
+++ readme.md
@@ -0,0 +1,5 @@
+- Follow Path Behavior
+//* this function make the model follow the path but not strict
+
+- On Path Behavior
+//* this function make the model strictly follow the line without approximation
